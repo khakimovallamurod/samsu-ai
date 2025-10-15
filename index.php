3e57cea1488c39af56324a6b1f9d7ab280def866
khakimovallamurod <allamurod0206@gmail.com>
2025-10-15 23:07:08 +0500

    add footer

--- index.php
+++ index.php
@@ -98,6 +98,9 @@
                 </button>
             </div>
         </div>
+        <footer style="text-align:center; padding:10px; font-size:14px; color:#777;">
+            &copy; 2025 Sun'iy intellekt va raqamli texnologiyalar fakulteti(SAMSU) tomonidan ishlab chiqildi.
+        </footer>
     </div>
     <script src="assets/js/script.js"></script>
 </body>
